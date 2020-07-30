b9c18787d821fc7169839655804aa8a5eb3c5c01
Monique.Bass01 <monique.bass01@gmail.com>
2020-07-30 17:37:23 -0500

    Hello World Works

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HelloWorld
 {
@@ -9,6 +10,34 @@ namespace HelloWorld
             Console.WriteLine("Howdy Y'all!!");
             //Console.ReadKey(); // What happens if you run the app without this line?;
 
+           
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Shark" };
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+           Console.WriteLine("These are the animals with more than 2 Syllables.");
+            foreach (var animal in animals)
+
+            {
+              var SyllableCount = 0;
+
+                foreach(var c in animal)
+                {
+                    if (vowels.Contains(c))
+                    {
+                       
+                        SyllableCount++;
+                         
+                    }
+                    
+                 }
+               
+                if (SyllableCount >=2)                  
+                {
+                    Console.WriteLine(animal);
+                 }
+
+
+            }
             Console.WriteLine("Please choose a greeting: 'Hello', 'GoodMorning', 'Hi Student'");
             var greeting = Console.ReadLine();
             Console.WriteLine($"I choose {greeting}!");
