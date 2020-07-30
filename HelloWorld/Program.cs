fd486be47fe2bf8adc517f334133d11157b30944
Monique.Bass01 <monique.bass01@gmail.com>
2020-07-30 12:33:57 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+            //Console.ReadKey(); // What happens if you run the app without this line?;
+
+            Console.WriteLine("Please choose a greeting: 'Hello', 'GoodMorning', 'Hi Student'");
+            var greeting = Console.ReadLine();
+            Console.WriteLine(greeting);
+
+        }
+    }
+}
