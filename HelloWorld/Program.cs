ad45687a792195b88b139a8373aae8ae36a6987e
Monique.Bass01 <monique.bass01@gmail.com>
2020-07-30 18:20:48 -0500

    console input complete

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,10 +9,19 @@ namespace HelloWorld
         {
             Console.WriteLine("Howdy Y'all!!");
             //Console.ReadKey(); // What happens if you run the app without this line?;
+            Console.WriteLine("Please enter your name.");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}.");
 
-           
+            
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Shark" };
             var vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+            Console.WriteLine("What is your favorite color?");
+            var myFavColor = Console.ReadLine();
+            int randomAnimal = new Random().Next(animals.Length);
+
+            Console.WriteLine($"Would you like a {myFavColor} {animals[randomAnimal]}? ");
 
            Console.WriteLine("These are the animals with more than 2 Syllables.");
             foreach (var animal in animals)
